d21e8314cebd93ed13aede38ac1cf6b3adf5a833
HanFengA7 <tancan137@foxmail.com>
2020-07-29 23:27:14 +0800

    使用PHP模板引擎”

--- usr/themes/main.php
+++ usr/themes/main.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="zh-cn">
-<?php 
-require(Usr_T_PATH.'/head.php');
-require(Usr_T_PATH.'/header.php');
-?>
-    <body class="<?php echo($page['body_class']); ?>">
-        <?php require(Usr_T_PATH.'/list.php'); ?>
-        <?php require($page['body_file']); ?>
-        <?php require(Usr_Tpi_PATH.'dialog.inc.html'); ?>
-        <?php require(Usr_T_PATH.'footer.php'); ?>
+<?include(Usr_T_PATH.'head.html')?>
+<?include(Usr_T_PATH.'header.html')?>
+    <body class="<?=$page['body_class']?>">
+        <?include(Usr_T_PATH.'list.html')?>
+        <?include($page['body_file'])?>
+        <?include(Usr_Tpi_PATH.'dialog.inc.html')?>
+        <?include(Usr_T_PATH.'footer.html')?>
     </body>
 </html>
