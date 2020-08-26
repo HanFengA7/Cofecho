db59bd5ae327a522de9545dc23ce51dc7eb337b8
HanFengA7 <tancan137@foxmail.com>
2020-08-26 11:35:09 +0800

    up Cofecho 0.1(2020826)

--- var/config/routes.php
+++ var/config/routes.php
@@ -4,6 +4,8 @@ $Suffix['2'] = '.html';
 
 $router->all('',(Usr_C_PATH.'Index/index'.$Suffix['1']));
 
+$router->all('AC',(Usr_C_PATH.'AC/ac'.$Suffix['1']));
+
 $router->get('Panel',(Usr_C_PATH.'Panel/index'.$Suffix['1']));
 $router->all('Panel/AC-Write',(Usr_C_PATH.'Panel/AC-write'.$Suffix['1']));
 $router->get('Panel/AC-List',(Usr_C_PATH.'Panel/AC-list'.$Suffix['1']));
@@ -11,5 +13,3 @@ $router->get('Panel/AC-List',(Usr_C_PATH.'Panel/AC-list'.$Suffix['1']));
 $router->all('User/Login',(Usr_C_PATH.'User/login'.$Suffix['1']));
 $router->get('User/Logout',(Usr_C_PATH.'User/logout'.$Suffix['1']));
 $router->get('User/Info',(Usr_C_PATH.'User/info'.$Suffix['1']));
-
-$router->all('AC',(Usr_C_PATH.'AC/ac'.$Suffix['1']));
